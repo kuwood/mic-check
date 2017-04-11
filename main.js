8a6265ce51fc5d46703c1649273febcb70e9c36c
Keith Underwood <keithunderwood510@gmail.com>
2017-04-10 18:33:31 -0700

    Add settings window show/hide functionality

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 let tray = null
+let willQuitApp = false
 
 // function createWindow () {
 //   // Create the browser window.
@@ -55,17 +56,25 @@ app.on('ready', () => {
   win.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
+  win.on('close', (e) => {
+    if (willQuitApp) {
+      // the user tried to quit the app 
+      window = null;
+    } else {
+      // the user only tried to close the window
+      e.preventDefault();
+      win.hide();
+    }
   })
 
   // set up the tray
   tray = new Tray('/home/kuwood/projects/mic-check/icon.png')
   const contextMenu = Menu.buildFromTemplate([
-    {label: 'Listen', type: 'checkbox', click(){ListState()}}
+    {label: 'Listen', type: 'checkbox', click(){ListenState()}},
+    {type: 'separator'},
+    {label: 'Settings', type: 'normal', click(){win.show()}},
+    {type: 'separator'},
+    {label: 'Quit', role: 'quit'}
   ])
 
   tray.setToolTip('This is my application.')
@@ -75,24 +84,25 @@ app.on('ready', () => {
   
   // Call this again for Linux because we modified the context menu
   tray.setContextMenu(contextMenu)
-  ipcMain.on('listen', () => {
-    
-  })
 
-  function ListState() {
+  function ListenState() {
     win.webContents.send('listen', contextMenu.items[0].checked)
   }
 
 })
 
+// 'before-quit' is emitted when Electron receives 
+// the signal to exit and wants to start closing windows
+app.on('before-quit', () => willQuitApp = true)
+
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+// app.on('window-all-closed', () => {
+//   // On macOS it is common for applications and their menu bar
+//   // to stay active until the user quits explicitly with Cmd + Q
+//   if (process.platform !== 'darwin') {
+//     app.quit()
+//   }
+// })
 
 // app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
